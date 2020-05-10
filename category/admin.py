c5feca96271a4094308ed4f53e7f0282ee91554c
Tumise <oladele.alade@gmail.com>
2020-05-10 05:57:11 +0100

    a couple new updates

--- category/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import CourseCategory
-# Register your models here.
-
-class CourseCategoryAdmin(admin.ModelAdmin):
-    readonly_fields =['slug']
-
-admin.site.register(CourseCategory,CourseCategoryAdmin)
